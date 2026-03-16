990e7e233aab28afacb24c750dc81a3979cdcf40
Mikhail Kominov <kamikadze66@narod.ru>
2026-03-16 15:06:10 +0500

    hw-5

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import checker from 'vite-plugin-checker'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { checker } from 'vite-plugin-checker';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
@@ -14,4 +14,4 @@ export default defineConfig({
       overlay: { initialIsOpen: true }
     })
   ],
-})
+});
